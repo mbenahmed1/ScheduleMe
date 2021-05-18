b153b30e52bcd8af927428d53ab714e4ec0f6a3d
Tim Adam <tadam@uos.de>
2021-05-18 23:27:57 +0200

    added file structure for the schedule_generator and solver

---
+++ scheduler/src/schedule_generator.cpp
@@ -0,0 +1,4 @@
+//
+// Created by tim on 18.05.21.
+//
+#include "../include/schedule_generator.h"
---
+++ scheduler/src/solver.cpp
@@ -0,0 +1,12 @@
+//
+// Created by tim on 18.05.21.
+//
+
+#include "../include/solver.h"
+
+#include <iostream>
+
+int main(int argc, char** argv)
+{
+    std::cout << "Solver" << std::endl;
+}
