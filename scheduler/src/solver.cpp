c571ce35e5fc53cd6756583c354babb9614db0bf
Mbenahmed <mbenahmed@uos.de>
2021-05-24 22:29:23 +0200

    fixed include path

--- scheduler/src/solver.cpp
+++ scheduler/src/solver.cpp
@@ -2,11 +2,15 @@
 // Created by tim on 18.05.21.
 //
 
-#include "../include/solver.h"
+#include "../include/io.hpp"
+#include "../include/solver.hpp"
 
 #include <iostream>
 
 int main(int argc, char** argv)
 {
     std::cout << "Solver" << std::endl;
+
+    uosrcp::Instance instance = uosrcp::read_instance("../../instances/j30/J301_1.RCP");
+
 }
