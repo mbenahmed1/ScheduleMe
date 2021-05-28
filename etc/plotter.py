07e02d7f64960ab9f648bf859325882301f6b5d4
Mbenahmed <mbenahmed@uos.de>
2021-05-28 19:21:08 +0200

    refactored plotter

--- etc/plotter.py
+++ etc/plotter.py
@@ -1,103 +1,144 @@
+""" plotter.py
+
+This file contains a plotter for rcpsp solutions. It reads files in the following format:
+
+
+n r
+[cap]^r
+id start proc [demand]^r
+...
+...
+
+with 
+    n:=         number of activities
+    r:=         number of resources
+    cap:=       capacity per resource
+
+and each line after the second representing an activity with
+
+    id:=        the id
+    start:=     starting time
+    proc:=      processing time
+    demand:=    the demand of resource r of activity id
+
+Example:
+
+3 2
+5 10
+1 1 3 1 0
+2 0 2 0 3
+3 3 3 0 2
+"""
+
 import matplotlib.pyplot as plt
 from matplotlib.patches import Rectangle
-import seaborn as sns
 import sys
-from operator import itemgetter
 from matplotlib.pyplot import cycler
 import numpy as np
 import matplotlib
-import random
-import re
 import time
-import subprocess
+
 
 class Act:
+    """Class that represents an Activity."""
+
     def __init__(self, _id: int, start: int, proc: int, demand: int, y_pos: int):
+        """Inits an instance of Act class.
+
+        Args:
+            _id:                id of the Activity.
+            start:              starting time.
+            proc:               processing time.
+            demand:             demand of a certain resource.
+            y_pos:              y position.
+        """
         self._id = _id
         self.start = start
         self.proc = proc
         self.demand = demand
         self.y_pos = y_pos
+
     def __str__(self):
+        """Creates a string of a given Activity.
+
+        Return:
+            String that represents the Activities attributes.
+        """
         return(f'ID: {self._id} start: {self.start} proc: {self.proc} demand: {self.demand} y_pos: {self.y_pos}')
 
 
 def check_rect_feaseability(act: Act, rsc_cap: int) -> bool:
+    """Checks if Activity has resource feaseable position.
+
+    Args:
+        act:                Activity to check.
+        rsc_cap:            Capacity of the corresponding resource.
+    Return:
+        True if feaseable, False if not.
+    """
     rect = Rectangle((act.start, act.y_pos), act.proc, act.demand)
     return not ((rect.get_y() + rect.get_height()) > rsc_cap)
 
-def check_y_pos(act: Act, acts: list, cap: int) -> bool:
-    rect = Rectangle((act.start, act.y_pos), act.proc, act.demand)
-    for i in range(len(acts)):
-        other_rect = Rectangle((acts[i].start, acts[i].y_pos), acts[i].proc, acts[i].demand)
-        if other_rect != rect:
-            if check_overlap(other_rect, rect):
-                #print('Geht nicht !!!')
-                x, y = rect.get_xy()
-                y += 1
-                rect.set_y(y)
-                if check_overlap(other_rect, rect):
-                    #print('Höher nicht !!!')
-                    return False
-    return True
 
-def check_y_pos_2(act: Act, acts: list, cap: int) -> bool:
+def check_y_pos(act: Act, acts: list) -> bool:
+    """Checks if the y position of Activities overlaps any other given Activities.
+
+    Args:
+        act:                Activity of which to check the overlap.
+        acts:               List of Activities to check against.
+    Returns:
+        True if Activity does not overlap with any of the other Activities.
+        False if not.
+    """
     rect = Rectangle((act.start, act.y_pos), act.proc, act.demand)
     for i in range(len(acts)):
-        other_rect = Rectangle((acts[i].start, acts[i].y_pos), acts[i].proc, acts[i].demand)
+        other_rect = Rectangle(
+            (acts[i].start, acts[i].y_pos), acts[i].proc, acts[i].demand)
         if other_rect != rect:
             if check_overlap(other_rect, rect):
                 return False
     return True
 
-def plot_acts(acts: list, plotted_acts: list, cap: int, start_id: int, ct: int):
 
-    #print(f'len {len(acts)} s_id {start_id}')
-    # new_rect = Rectangle((acts[start_id].start, acts[start_id].y_pos), acts[start_id].proc, acts[start_id].demand, facecolor="grey", edgecolor='black')
-    # print(ct)
-    # print(len(plotted_acts))
-    # print(start_id, acts[start_id]._id, acts[start_id].y_pos)
-    if check_y_pos_2(acts[start_id], plotted_acts, cap):
+def plot_acts(acts: list, plotted_acts: list, cap: int, start_id: int) -> None:
+    """Recursively calls itself to serach for feaseable Rectangle positions.
+
+    Args:
+        acts:               List of all activities of one resource.
+        plotted_acts:       List of all activities that are scheduled.
+        cap:                Maximum capacity of the resource.
+        start_id:           ID of the chosen activity.
+    """
+    if check_y_pos(acts[start_id], plotted_acts):
         plotted_acts.append(acts[start_id])
-        # print(start_id, acts[start_id]._id, "fitted")
-        # for act in plotted_acts:
-        #     print(act)
         if start_id < (len(acts) - 1):
-            if not plot_acts(acts, plotted_acts, cap, start_id + 1, ct + 1):
+            if not plot_acts(acts, plotted_acts, cap, start_id + 1):
                 plotted_acts.pop()
                 acts[start_id].y_pos += 1
                 if check_rect_feaseability(acts[start_id], cap):
-                    return plot_acts(acts, plotted_acts, cap, start_id, ct + 1)
+                    return plot_acts(acts, plotted_acts, cap, start_id)
                 else:
-                    # print(start_id, acts[start_id]._id, "infeasable")
                     acts[start_id].y_pos = 0
                     return False
-
         return True
-        #print("+-0")
     else:
-        # print(start_id, acts[start_id]._id, "y not")
         acts[start_id].y_pos += 1
         if check_rect_feaseability(acts[start_id], cap):
-            # print(start_id, acts[start_id]._id, "feasable")
-            # plotted_acts.append(acts[start_id])
-            return plot_acts(acts, plotted_acts, cap, start_id, ct + 1)
-            #print("+1")
+            return plot_acts(acts, plotted_acts, cap, start_id)
         else:
-            # print(start_id, acts[start_id]._id, "infeasable")
             acts[start_id].y_pos = 0
-            #print("-1")
             return False
-    
-    
-    # if len(plotted_acts) != len(acts):
-    #     if start_id < (len(acts) - 1):
-    #         plot_acts(acts, plotted_acts, cap, start_id + 1)
-    #         print("next")
 
 
 def check_overlap(rect_a: Rectangle, rect_b: Rectangle) -> bool:
+    """Checks if two Rectangles overlap eachother.
 
+    Args:
+        rect_a:             First Rectangle to check.
+        rect_b:             Second Rectangle to check.
+    Return:
+        True if Rectangles overlap, False if not.
+    """
     a_x_bottom_left, a_y_bottom_left = rect_a.get_xy()
     b_x_bottom_left, b_y_bottom_left = rect_b.get_xy()
 
@@ -118,40 +159,21 @@ def check_overlap(rect_a: Rectangle, rect_b: Rectangle) -> bool:
 
     return not (a_x_top_right <= b_x_bottom_left or a_x_bottom_left >= b_x_top_right or a_y_top_right <= b_y_bottom_left or a_y_bottom_left >= b_y_top_right)
 
-def check_overlap_rsc(rect_a: Rectangle, rects_in_rsc: list) -> bool:
-    for rect in rects_in_rsc:
-        return check_overlap(rect_a, rect)
-    return False
 
-def check_x_conf(rect_a: Rectangle, rect_b: Rectangle) -> bool:
-    x_a, y_a = rect_a.get_xy()
-    x_b, y_b = rect_b.get_xy()
-
-    w_a = rect_a.get_width()
-    w_b = rect_b.get_width()
-
-    dist = []
-    dist.append(abs(x_a - x_b + w_b))
-    dist.append(abs(x_a - x_b))
-    dist.append(abs(x_b - x_a + w_a))
-    dist.append(abs(x_b - x_a))
-
-    span = max(dist)
-
-    return (span - w_a - w_b) < 0
-    
-
-def print_usage():
+def print_usage() -> None:
+    """Prints the usage to console."""
     print('usage: $ python3 plotter.py <PLOTFILE_PATH> ')
 
 
+def parse_files(sol_path: str) -> None:
+    """Parses the given path and starts the plotting process.
 
-
-def parse_files(sol_path: str):
+    Args:
+        sol_path:               Path to the solution file.
+    """
     caps = []
-    num_act = -1
-    num_rsc = -1
     end_times = []
+    rects = []
 
     with open(sol_path) as s_file:
         lines = s_file.readlines()
@@ -159,65 +181,69 @@ def parse_files(sol_path: str):
         first = first.split()
         num_act = int(first[0])
         num_rsc = int(first[1])
-
         colors = plt.cm.Pastel2(np.linspace(0, 1, num_act))
+        fig, ax = plt.subplots(num_rsc)
 
         second = lines[1]
         second = second.split()
-        for act in range(num_rsc):
-            caps.append(int(second[act]))
 
-        rects = {}
+        for i, rsc in enumerate(second):
+            caps.append(int(rsc))
+
+        acts = {}
+        plotted_acts = {}
+
         for rsc in range(num_rsc):
-            rects[rsc] = []
-
-        fig, ax = plt.subplots(num_rsc)
-
-        feaseable = True
-
+            acts[rsc] = []
+            plotted_acts[rsc] = []
         sorted_lines = sorted(
             lines[2:], key=lambda x: x.split()[1], reverse=False)
-
-        _ys = []
 
         for line in sorted_lines:
             line = line.split()
             _id = int(line[0])
             start = int(line[1])
             proc = int(line[2])
+            for rsc in range(num_rsc):
+                demand = int(line[rsc + 3])
+                if demand > 0:
+                    act = Act(_id, start, proc, demand, 0)
+                    acts[rsc].append(act)
+        start = time.time_ns()
 
-            for i, demand in enumerate(line[3:]):
-                if int(demand) != 0:
+        for rsc in range(num_rsc):
+            acts[rsc] = sorted(acts[rsc], key=lambda x: x.start, reverse=False)
 
-                    rect = Rectangle((start, 0), proc, int(
-                        demand), facecolor=colors[_id], edgecolor='black')
+        for rsc in range(num_rsc):
+            plot_acts(acts[rsc], plotted_acts[rsc], caps[rsc], 0)
 
-                    end_times.append(start + proc)
+        secs = (time.time_ns() - start) / 1000000000.0
+        print(f'Found plot in {round(secs, 3)} s.')
 
-                    _y0 = 0
-                    _y = _y0
+        for rsc in range(num_rsc):
+            if not (len(acts[rsc]) == len(plotted_acts[rsc])):
+                print(
+                    f"Error: Resource {rsc} does not contain all activities!")
+                sys.exit(-1)
+            else:
+                print(f"Resource {rsc} ok.")
 
-                    for r in rects[i]:
-                        if r != rect:
-                            while check_overlap(r, rect):
-                                _y += 1
-                                rect.set_y(_y)
-  
-                    rects[i].append(rect)
-
-                    rect_list = {str(_id): rect}
-                    ax[i].add_artist(rect_list[str(_id)])
-                    rx, ry = rect_list[str(_id)].get_xy()
-                    cx = rx + rect_list[str(_id)].get_width()/2.0
-                    cy = ry + rect_list[str(_id)].get_height()/2.0
-                    ax[i].annotate(str(_id), (cx, cy), color='black',
-                                   fontsize=8, ha='center', va='center')
-                    ax[i].title.set_text(f'Resource {i}')
+        for rsc in range(num_rsc):
+            for act in plotted_acts[rsc]:
+                new_rect = Rectangle((act.start, act.y_pos), act.proc,
+                                     act.demand, facecolor=colors[act._id], edgecolor='black')
+                end_times.append(act.start + act.proc)
+                ax[rsc].add_artist(new_rect)
+                rx, ry = new_rect.get_xy()
+                cx = rx + new_rect.get_width()/2.0
+                cy = ry + new_rect.get_height()/2.0
+                ax[rsc].annotate(act._id, (cx, cy), color='black',
+                                 fontsize=8, ha='center', va='center')
 
         y_range = range(0, max(caps), 1)
         x_range = range(0, max(end_times), 1)
         for i, a in enumerate(ax):
-            # a.grid()
+            a.title.set_text(f'Resource {i}')
             a.set(xlim=(-0.1, max(end_times) + 1), ylim=(-0.1,
                                                          max(caps) + 1), yticks=y_range, xticks=x_range)
             a.axline((-0.1, caps[i]), (max(end_times),
@@ -225,102 +251,10 @@ def parse_files(sol_path: str):
 
 
 if __name__ == "__main__":
-    print_usage()
-    file = "~/uni/10sem/rcpsp/ScheduleMe/scheduler/build/test.lol"
     if len(sys.argv) > 1:
-        file = sys.argv[1]
-    # file = sys.argv[1]
-    # parse_files(file)
-    # plt.show()
-
-    # neuer versuch
-    caps = []
-    end_times = []
-    rects = []
-
-    with open(file) as s_file:
-        lines = s_file.readlines()
-        first = lines[0]
-        first = first.split()
-        num_act = int(first[0])
-        num_rsc = int(first[1])
-        colors = plt.cm.Pastel2(np.linspace(0, 1, num_act))
-        
-        fig, ax = plt.subplots(num_rsc)
-
-        second = lines[1]
-        second = second.split()
-        for i, rsc in enumerate(second):
-            caps.append(int(rsc))
-        
-        acts = {}
-        plotted_acts = {}
-        for rsc in range(num_rsc):
-            acts[rsc] = []
-            plotted_acts[rsc] = []
-        
-        sorted_lines = sorted(lines[2:], key=lambda x: x.split()[1], reverse=False)
-
-        for line in sorted_lines:
-            line = line.split()
-            _id = int(line[0])
-            start = int(line[1])
-            proc = int(line[2])
-        
-            for rsc in range(num_rsc):
-                demand = int(line[rsc + 3])
-                if demand > 0:
-                    act = Act(_id, start, proc, demand, 0)
-                    acts[rsc].append(act)
-
-
-        # for rsc in range(num_rsc):
-        #     print(f"RESOURCE >>> {rsc}")
-        #     for act in acts[rsc]:
-        #         print(act._id)
-        # sys.exit()
-
-        start = time.time_ns()
-
-        for rsc in range(num_rsc):
-            acts[rsc] = sorted(acts[rsc], key=lambda x: x.start, reverse=False)
-
-        for rsc in range(num_rsc):
-            # print(f"\n<<<<< Now plotting resource {rsc} >>>>>\n")
-            plot_acts(acts[rsc], plotted_acts[rsc], caps[rsc], 0, 0)
-            # plt.show()
-
-        print((time.time_ns() - start) / 1000000000.0)
-
-        for rsc in range(num_rsc):
-            if not (len(acts[rsc]) == len(plotted_acts[rsc])):
-                print(f"Resource {rsc} does not contain all activities!")
-                sys.exit(-1)
-            else:
-                print(f"Resource {rsc} okay.")
-
-        for rsc in range(num_rsc):
-            for act in plotted_acts[rsc]:
-                new_rect = Rectangle((act.start, act.y_pos), act.proc, act.demand, facecolor=colors[act._id], edgecolor='black')
-                
-                end_times.append(act.start + act.proc)
-
-                # print(act)
-
-                ax[rsc].add_artist(new_rect)
-                rx, ry = new_rect.get_xy()
-                cx = rx + new_rect.get_width()/2.0
-                cy = ry + new_rect.get_height()/2.0
-                ax[rsc].annotate(act._id, (cx, cy), color='black',
-                                fontsize=8, ha='center', va='center')
-        
-        y_range = range(0, max(caps), 1)
-        x_range = range(0, max(end_times), 1)
-        for i, a in enumerate(ax):
-            # a.grid()
-            a.set(xlim=(-0.1, max(end_times) + 1), ylim=(-0.1,
-                                                        max(caps) + 1), yticks=y_range, xticks=x_range)
-            a.axline((-0.1, caps[i]), (max(end_times),
-                                    caps[i]), ls='--', lw=1.5, color='black')
-
-    plt.show()
+        parse_files(sys.argv[1])
+        plt.show()
+        sys.exit(-1)
+    else:
+        print_usage()
+        sys.exit(-1)
