98c8b00900598a3c762f77a4e0896fc363af8dfe
David <david.massanes@gmx.de>
2021-05-30 15:11:25 +0200

    added directorys for the solutions - minor changes to tuning script

--- etc/parameter_tuning.py
+++ etc/parameter_tuning.py
@@ -1,4 +1,4 @@
-import subprocess
+import subprocess as sp
 import multiprocessing
 import argparse
 import sys
@@ -8,19 +8,28 @@ import re
 if __name__ == "__main__":
     cpu_ct  = multiprocessing.cpu_count()
 
-    times               = [i for i in range(1, 31) if i % 5 == 0 or i == 1]
+    seeds               = [0]
+    times               = [i / 2 for i in range(1, 11)]
     n_processes         = cpu_ct
     n_processes         = 1
     solver_path         = os.path.abspath("../scheduler/build/solver")
-    instances_path      = os.path.abspath("../instances/j120/")
+    instances_path      = os.path.abspath("../instances/j120")
     instance_regex      = ".*\.RCP"
     instance_regex      = "[A-Z][1-9]_.*\.RCP"
     # instance_regex      = "[A-Z]+[5]|[1-9][5-9]_[0-9].RCP"
+    solutions_path      = os.path.abspath("../solutions/j120")
 
+    solutions = []
     instances = []
-    for instance in os.listdir(instances_path):
-        if re.match(instance_regex, instance):
-            instances.append(instance)
+    for inst in os.listdir(instances_path):
+        if re.match(instance_regex, inst):
+            inst_path   = os.path.join(instances_path, inst)
+            sol_path    = os.path.join(solutions_path, inst[:-4] + ".sol")
+            instances.append(inst_path)
+            solutions.append(sol_path)
+    if len(instances) != len(solutions):
+        print("len(instances) != len(solutions)")
+        sys.exit(-1)
     nb_instances = len(instances)
 
     print(f"times               {times}")
@@ -31,8 +40,12 @@ if __name__ == "__main__":
     print(f"instance_regex      {instance_regex}")
     print(f"nb_instances        {nb_instances}")
 
+    for i in range(nb_instances):
+        for t in times:
+            # ./solver <instance-path> <solution-path> <time-limit> <seed> [OPTION]...
+            command = f"{solver_path} {instances[i]} {solutions[i]} {t}"
+            # print(command)
 
     # print(command)
-    # sp = subprocess.Popen(command, shell=True)
-    # sp_gensdp = subprocess.Popen(command, shell=True, stdout=logfile_gensdp, stderr=logfile_gensdp, start_new_session=True)
+    # p = sp.Popen(command, shell=True)
 
